407f43fa119e9ece195d967d725f674adcb89f1f
androi7 <gabriel.hilti@gmail.com>
2022-10-07 01:34:01 +0200

    refactor: clean up wrong git pull

--- lib/src/level/domain/i_level_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:sozzle/src/level/domain/level_data.dart';
-
-// load/save leveldata
-abstract class ILevelRepository {
-  Future<LevelData> getLevel(int id);
-  Future<void> setLevel(LevelData level);
-}
